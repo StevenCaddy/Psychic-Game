a3e2b7fb9e9326eccb89493be88e2e9bc2769d2f
Steven Cadwallader <steven.caddie@gmail.com>
2017-07-08 18:59:30 -0500

    rough draft

---
+++ assets/javascript/game.js
@@ -0,0 +1,36 @@
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var winsCounter = 0;
+var lossesCounter = 0;
+var guesses = [];
+var guessesLeft = 9;
+var letterToGuess = letters[Math.floor(Math.random() * letters.length)];
+
+reset();
+
+document.onkeydown = function(event) {
+  var guess = event.key;
+  if (guess === letterToGuess) {
+    reset();
+    winsCounter++;
+    var wins = document.getElementById("wins");
+    wins.innerHTML = winsCounter;
+  } else if (guessesLeft - 1 === 0) {
+    reset();
+    lossesCounter++;
+    var losses = document.getElementById("losses");
+    losses.innerHTML = lossesCounter;
+  } else {
+    guessesLeft--;
+    var guessed = document.getElementById("guesses");
+    guessed.innerHTML = guesses;
+    guesses.push(guess);
+  }
+}
+
+function reset() {
+  guessesLeft = 9;
+  guesses = [];
+  letterToGuess = letters[Math.floor(Math.random() * letters.length)];
+  console.log("Letter to guess: " + letterToGuess);
+}
+
